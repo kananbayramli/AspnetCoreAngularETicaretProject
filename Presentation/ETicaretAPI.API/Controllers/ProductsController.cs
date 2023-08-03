098401bc38114933412243e5340d2fccc01f7a41
kananbayramli <bayramli-kenan@mail.ru>
2023-08-03 15:54:29 +0400

    EFCore Tracking Optimization

--- Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
+++ Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 ﻿using ETicaretAPI.Application.Repositories;
+using ETicaretAPI.Domain.Entities;
 using ETicaretAPI.Persistence.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
@@ -17,22 +18,37 @@ namespace ETicaretAPI.API.Controllers
             _productReadRepository = productReadRepository;
         }
 
+
         [HttpGet]
 
-        public async void Get() 
+        public async Task Get()
         {
-            await _productWriteRepository.AddRangeAsync(new()
-            { 
-                new() { Id = Guid.NewGuid(), Name= "Product 1", CreatedDate=DateTime.UtcNow, Price = 100, Stock = 10 },
-                new() { Id = Guid.NewGuid(), Name= "Product 2", CreatedDate=DateTime.UtcNow, Price = 200, Stock = 20 },
-                new() { Id = Guid.NewGuid(), Name= "Product 3", CreatedDate=DateTime.UtcNow, Price = 300, Stock = 30 }
-            });
-             var count = await _productWriteRepository.SaveAsync();
-        
+            //await _productWriteRepository.AddRangeAsync(new()
+            //{
+            //    new() { Id = Guid.NewGuid(), Name= "Product 1", CreatedDate=DateTime.UtcNow, Price = 100, Stock = 10 },
+            //    new() { Id = Guid.NewGuid(), Name= "Product 2", CreatedDate=DateTime.UtcNow, Price = 200, Stock = 20 },
+            //    new() { Id = Guid.NewGuid(), Name= "Product 3", CreatedDate=DateTime.UtcNow, Price = 300, Stock = 30 }
+            //    });
+            //var count = await _productWriteRepository.SaveAsync();
+
+            Product p = await _productReadRepository.GetByIdAsymc("e1d2608f-2a94-43cf-84e2-5a5111b5a1b8", false); 
+            //Deyishikliklere icaze vermir
+            p.Name = "kanan";
+            await _productWriteRepository.SaveAsync();
+
         }
 
 
+        [HttpGet("{id}")]
+
+        public async Task<IActionResult> Get(string id)
+            {
+                Product product = await _productReadRepository.GetByIdAsymc(id);
+                return Ok(product);
+            }
+
+
+
+        }
 
     }
-
-}
