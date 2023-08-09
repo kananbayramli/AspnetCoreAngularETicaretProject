97e6e6b7e3d092dc9712f1ae607cb1b92e7e834f
kananbayramli <bayramli-kenan@mail.ru>
2023-08-10 01:28:01 +0400

    Building File Upload Mechanism

--- Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
+++ Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
@@ -14,16 +14,17 @@ namespace ETicaretAPI.API.Controllers
     {
         readonly private IProductWriteRepository _productWriteRepository;
         readonly private IProductReadRepository _productReadRepository;
-
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
         public ProductsController(
             IProductWriteRepository productWriteRepository,
-            IProductReadRepository productReadRepository)
+            IProductReadRepository productReadRepository,
+            IWebHostEnvironment webHostEnvironment)
 
         {
             _productWriteRepository = productWriteRepository;
             _productReadRepository = productReadRepository;
-
+            this._webHostEnvironment = webHostEnvironment;
         }
 
 
@@ -103,6 +104,27 @@ namespace ETicaretAPI.API.Controllers
             await _productWriteRepository.SaveAsync();
             return Ok();
 
+        }
+
+        [HttpPost("[action]")]
+        public async Task<IActionResult> Upload() 
+        {
+            string uploadPath = Path.Combine(_webHostEnvironment.WebRootPath, "resource/product-images");
+
+            if (!Directory.Exists(uploadPath))
+                Directory.CreateDirectory(uploadPath);
+
+            Random r = new();
+
+            foreach (IFormFile file in Request.Form.Files)
+            {
+                string fullPath = Path.Combine(uploadPath, $"{r.Next()}{Path.GetExtension(file.FileName)}");
+
+                using FileStream fileStream = new(fullPath, FileMode.Create, FileAccess.Write, FileShare.None, 1024 * 1024, useAsync: false);
+                await file.CopyToAsync(fileStream);
+                await fileStream.FlushAsync();
+            }
+            return Ok();
         }
 
     }
