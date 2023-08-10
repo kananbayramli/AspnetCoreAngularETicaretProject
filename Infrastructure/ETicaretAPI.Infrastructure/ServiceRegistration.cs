7b7ff5c4409e444b58d8669632875f1bd76bc8a8
kananbayramli <bayramli-kenan@mail.ru>
2023-08-10 19:57:53 +0400

    Architecturally physical File Recording

---
+++ Infrastructure/ETicaretAPI.Infrastructure/ServiceRegistration.cs
@@ -0,0 +1,18 @@
+﻿using ETicaretAPI.Application.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Infrastructure
+{
+    public static class ServiceRegistration
+    {
+        public static void AddInfrastructureServices(this IServiceCollection serviceCollection) 
+        {
+            serviceCollection.AddScoped<IFileService, IFileService>();
+        }
+    }
+}
--- Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
+++ Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using ETicaretAPI.Application.ViewModels.Products;
 using System.Net;
 using ETicaretAPI.Application.RequestParametrs;
+using ETicaretAPI.Application.Services;
 
 namespace ETicaretAPI.API.Controllers
 {
@@ -15,16 +16,19 @@ namespace ETicaretAPI.API.Controllers
         readonly private IProductWriteRepository _productWriteRepository;
         readonly private IProductReadRepository _productReadRepository;
         private readonly IWebHostEnvironment _webHostEnvironment;
+        readonly IFileService _fileService;
 
         public ProductsController(
             IProductWriteRepository productWriteRepository,
             IProductReadRepository productReadRepository,
-            IWebHostEnvironment webHostEnvironment)
+            IWebHostEnvironment webHostEnvironment,
+            IFileService fileService)
 
         {
             _productWriteRepository = productWriteRepository;
             _productReadRepository = productReadRepository;
             this._webHostEnvironment = webHostEnvironment;
+            _fileService = fileService;
         }
 
 
@@ -109,21 +113,7 @@ namespace ETicaretAPI.API.Controllers
         [HttpPost("[action]")]
         public async Task<IActionResult> Upload() 
         {
-            string uploadPath = Path.Combine(_webHostEnvironment.WebRootPath, "resource/product-images");
-
-            if (!Directory.Exists(uploadPath))
-                Directory.CreateDirectory(uploadPath);
-
-            Random r = new();
-
-            foreach (IFormFile file in Request.Form.Files)
-            {
-                string fullPath = Path.Combine(uploadPath, $"{r.Next()}{Path.GetExtension(file.FileName)}");
-
-                using FileStream fileStream = new(fullPath, FileMode.Create, FileAccess.Write, FileShare.None, 1024 * 1024, useAsync: false);
-                await file.CopyToAsync(fileStream);
-                await fileStream.FlushAsync();
-            }
+            await _fileService.UploadAsync("resource/product-images", Request.Form.Files);
             return Ok();
         }
 
